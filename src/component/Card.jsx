6a9d4555de1ce7fffc5e3822e1c578961f75a15c
DevSohel32 <mdsujaudddoulsohel1@gmail.com>
2026-09-15 01:01:35 +0600

    Auto commit

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,6 +2,7 @@ export default function Card (props){
     console.log(props);
     return <div>
         <h1>Card Title {props.name}</h1>
-        
+                <h1>Card Title {props.name}</h1>
+
     </div>
 }
