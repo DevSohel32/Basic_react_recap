9f9c2b7b8621a89b589070332e951b73f01f033d
DevSohel32 <mdsujaudddoulsohel1@gmail.com>
2026-09-15 00:49:03 +0600

    Auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Card from './component/card';
 
 function App() {
   return (
-   <Card name='md sujauddoula sohel kire'/>
+   <Card name='md sujauddoula sohel kire'/>;
    <TestComponent/>
 
   )
