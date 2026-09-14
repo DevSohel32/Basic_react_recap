d1b1a2acd0ffcf8e15b4ebd3aa0a28fc27079d29
DevSohel32 <mdsujaudddoulsohel1@gmail.com>
2026-09-15 00:18:00 +0600

    Create react project and Componanted create rules

---
+++ src/TestComponent.jsx
@@ -0,0 +1,10 @@
+// const TestComponent =()=>{
+//     return <h1> Test Component create successfully
+//     </h1>
+// }
+
+// export default TestComponent;
+
+export default function TestComponent() {
+    return <h1>md sujauddoula sohel </h1>
+}
