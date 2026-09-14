5af078d91764676425f593c18175549268466bb1
DevSohel32 <mdsujaudddoulsohel1@gmail.com>
2026-09-15 01:03:38 +0600

    Auto commit

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,7 +3,8 @@ export default function Card (props){
     return <div>
         <h1>Card Title {props.name}</h1>
                 <h1>Card Title {props.name}</h1>
-                
+                                <h1>Card Title {props.name}</h1>
+
 
     </div>
 }
